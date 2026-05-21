f842aef6dd273cd8a0b0f66883ddfda54352c33e
LeonardoSoares41 <leonardosilvasoares32@gmail.com>
2026-05-21 20:59:36 -0300

    alteracao do arquivo eventos

--- JavaScript/eventos.js
+++ JavaScript/eventos.js
@@ -2,6 +2,8 @@ let inputmain = document.querySelector("#main-input");
 
 function btnMeuBotao() {
     alert("Botão clicado com sucesso!")
+    alert("O valor do input é: " + inputmain.value);
+    document.getElementById("input-value").textContent = inputmain.value;    
 }
 //document.getElementById("meuBotao").addEventListener("click", btnMeuBotao);
 
